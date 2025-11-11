ed5ccdc09c3dcb1a4cc532db762ccb2bc2138549
Lokeshgit-stack <lokesh142002mailid@gmail.com>
2025-11-12 00:22:38 +0530

    add dynamic api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+// Dynamically use correct API base URL
+const API_URL =
+  import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
